c5c8f6d5506ddaa82c5b1d35f5f3b5bc9cf8ef98
Hare Krishna <shivanshu singh>
2023-12-01 18:10:05 +0530

    Learn Packages

--- app.js
+++ app.js
@@ -1,24 +1,4 @@
-const http = require('http');
-
-const server = http.createServer((req,res)=>{
-    if(req.url === "/"){
-        res.end("Welcome to home page");
-        return
-    }
-    if(req.url == "/about"){
-        res.end("Welcome to about page")
-        return
-    }
-
-    res.end(`
-    
-    <h1>Oops!</h1>
-    <p>We can't seem to find page </p>
-    <a href="/">Go to Home</a>
-    
-    `)
-    
-
-})
-
-server.listen(5000)
+const _= require('lodash');
+const items = [1,[2,[3,[4]]]]
+const newItems = _.flattenDeep(items);
+console.log(newItems);

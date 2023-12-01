e591ac47f72aed8f3fb372e45b8dc1747edadc8c
Hare Krishna <shivanshu singh>
2023-12-01 17:52:13 +0530

    Leanr Http

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    if(req.url === "/"){
+        res.end("Welcome to home page");
+        return
+    }
+    if(req.url == "/about"){
+        res.end("Welcome to about page")
+        return
+    }
+
+    res.end(`
+    
+    <h1>Oops!</h1>
+    <p>We can't seem to find page </p>
+    <a href="/">Go to Home</a>
+    
+    `)
+    
+
+})
+
+server.listen(5000)

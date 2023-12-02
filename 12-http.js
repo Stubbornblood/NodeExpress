728e020e92874550f6ed07bd8c83a6800b92e7b1
Hare Krishna <shivanshu singh>
2023-12-02 14:29:29 +0530

    Event Loop

--- 12-http.js
+++ 12-http.js
@@ -21,4 +21,4 @@ const server = http.createServer((req,res)=>{
 
 })
 
-server.listen(5000)
+server.listen(5000) 
--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const _= require('lodash');
-const items = [1,[2,[3,[4]]]]
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+//Event Loop -> to perform non blocking input and output javascript is single thread
+
+//Part of Global object = which is window
+//WEB Apis : setTimeout, DOM api , fetch() , localStorage, console, location <- all part of browser not javascript//GE
+//GEC -> Global execution context
+
+console.log("Start");
+
+setTimeout(function cb(){
+    console.log("CallBack");
+},5000)
+console.log("End");

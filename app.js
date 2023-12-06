e7f738519221a3e57b24860931c2e7ed62f5c9c0
Hare Krishna <shivanshu singh>
2023-12-06 14:34:00 +0530

    Event loop 2

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 //Event Loop -> to perform non blocking input and output javascript is single thread
-
 //Part of Global object = which is window
 //WEB Apis : setTimeout, DOM api , fetch() , localStorage, console, location <- all part of browser not javascript//GE
 //GEC -> Global execution context
 //Callback Queue
 //Event Loop -> only job is to continusly monitor event loop and call stack if function waiting to executed it takes it and send it too call stack 
-
+//Micro-Task Queue -> with higher priority then callback queue fuction comes in it fist execute first
+//All function come through promices come and store in microtask queue 
+//Mutation Objser keep cheacking if there is some mutation in dome tree or not
 
 setTimeout(()=>{
 console.log("Hello i am learning event loop")
 },1000) 
+
